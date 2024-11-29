1083c27fba523e2ae4e6b1efd3864709753e9df0
rickky <peychart@users.noreply.github.com>
2024-11-28 16:27:37 -1000

    Create dateControl.js

---
+++ sources/public/js/dateControl.js
@@ -0,0 +1,13 @@
+function check(input) {
+    let value = input.value.replace(/[^0-9/]/g, '').replace(/\/+/g, '/');
+    if (
+        (value.length === 1 && parseInt(value.slice(0, 1), 10) > 3) ||
+        (value.length < 3 && parseInt(value.slice(0, 2), 10) > 31) ||
+        (value.length < 5 && parseInt(value.slice(3, 4), 10) > 1) ||
+        (value.length < 6 && parseInt(value.slice(3, 5), 10) > 12) ||
+        (value.length > 2 && value[2] !== '/') ||
+        (value.length > 5 && value[5] !== '/') ||
+        (value.length > 10)
+    ) value = value.slice(0, -1);
+    input.value = value;
+}
