b5f0aae740aab59f30898b34f9d613b2261bfb77
rickky <peychart@users.noreply.github.com>
2024-12-05 16:22:36 -1000

    Update popupMultiValues.js

--- sources/public/js/popupMultiValues.js
+++ sources/public/js/popupMultiValues.js
@@ -6,8 +6,9 @@ function initializePopup(input) {
 
 	let adding = false;
 	let options = JSON.parse(input.value || []); // Créer une copie des valeurs initiales
-	index(0); // Index de la position de la sélection
-	// Stocker le gestionnaire oninput existant, s'il y en a un
+	input.value = options[index(0)];
+
+	// Sauvegarde de l'event oninput existant, s'il y a
 	const originalOnInput = (typeof input.oninput === 'function') ?input.oninput :null;
 	input.removeAttribute('oninput');
 
@@ -16,26 +17,43 @@ function initializePopup(input) {
 	popupOptions.className = 'popup'; popupOptions.classList.add('hidden');
 	input.parentElement.appendChild(popupOptions);
 
+	// Gestion de l'index, position courante dans la popup
+	function index(i=-1) {
+		if (i >= 0)
+			input.setAttribute('data-index', i<options.length ?i :0);
+		return parseInt(input.getAttribute('data-index'), 10);
+	}
+
 	// Fonction pour rendre les options dans la popup
 	function renderOptions() {
 		popupOptions.innerHTML = ''; // Vider les options existantes
 		options.forEach((value, i) => {
 			const optionElement = document.createElement('div');
 			optionElement.className = 'option' + (i === index() ? ' selected' : ''); // Ajout de la classe 'selected' si c'est l'index actuel
-			optionElement.textContent = value;
+			optionElement.textContent = value.length ?value :'...';
 			popupOptions.appendChild(optionElement);
 		});
+		// Afficher la popup
+		const rect = input.getBoundingClientRect();
+		popupOptions.style.width = `${input.offsetWidth}px`;	// Ajuster la largeur sur celle de l'input
+		popupOptions.style.top = `${rect.bottom + window.scrollY - 45}px`; // juste sous le champ
+		setTimeout(() => {popupOptions.style.display = 'block';}, 0);
 	}
 
-	function index(i=-1) {
-		let index = input.getAttribute('data-index');
-		if (i >= 0) {index = i < options.length ?i :0; input.value = options[index] ?? '';}
-		input.setAttribute('data-index', index);
-		return parseInt(index, 10);
-	}
+	// Ouvrir la popup lorsque le champ input reçoit le focus
+	input.addEventListener('focus', () => {
+		options = JSON.parse(input.getAttribute('data-value'));
+		renderOptions();	// Rendre les options dans la popup
+
+if (input.name === 'telephoneNumber'){
+console.log('->focusEvent: ');
+console.log('telephoneNumber: ', input.getAttribute('data-value'));
+}
+
+	});
 
 	// Mise à jour de la valeur pointée par l'index en temps réel
-	function inputChanged() {
+	input.addEventListener('input', () => {
 		options[index()] = input.value; // Mettre à jour la valeur actuelle pointée par l'index
 		options = options.filter(item => item !== "");
 		if (index() >= options.lengt && options.length) index(options.length-1);
@@ -43,24 +61,12 @@ function initializePopup(input) {
 		renderOptions(); // Rendre les options pour mettre à jour
 
 		// Appeler le gestionnaire oninput de l'utilisateur, s'il est défini
-		if (originalOnInput)
+		if (originalOnInput) {
 			try {
 				originalOnInput(); // Appeler le gestionnaire oninput utilisateur
 			} catch (error) {
 				console.error('Erreur dans le gestionnaire oninput de l\'utilisateur:', error);
-	}		}
-	input.addEventListener('input', () => {inputChanged();});
-
-	// Ouvrir la popup lorsque le champ input reçoit le focus
-	input.addEventListener('focus', () => {
-		const values = JSON.parse(input.getAttribute('data-value') || "[]");
-		if (values.length > 1) {									// Vérifiez qu'il y a au moins 2 valeurs
-			renderOptions();										// Rendre les options dans la popup
-			const rect = input.getBoundingClientRect();
-			popupOptions.style.width = `${input.offsetWidth}px`;	// Ajuster la largeur pour qu'elle corresponde à l'input
-			popupOptions.style.top = `${rect.bottom + window.scrollY - 45}px`; // Positionner juste en dessous du champ
-			setTimeout(() => {popupOptions.style.display = 'block';}, 0);
-		} index();
+		}	}
 	});
 
 	// Sélection d'une option dans la popup
@@ -73,44 +79,48 @@ function initializePopup(input) {
 	});
 
 	// Fermer la popup si l'utilisateur clique en dehors
-	function exitInput() {
-		adding = false;
-		options = options.filter(item => item !== "");
-		input.setAttribute('data-value', JSON.stringify(options)); // Mettre à jour les multi-values
-		if (!input.value && options.length) index(0);
-		if (!popupOptions.contains(event.target)
-				&& !input.contains(event.target)
-		) popupOptions.style.display = 'none'; // Fermer la popup
-	} document.addEventListener('click', (event) => {exitInput();});
+	document.addEventListener('click', (event) => {
+		// Sur perte du focus, hidden la popup
+		if (!input.contains(event.target) && !popupOptions.contains(event.target)) {
+			adding = false;
+			options = options.filter(item => item !== "");	// Filtre les options vides
+			if (!input.value.length && options.length)
+				input.value = options[index(0)];
+			popupOptions.style.display = 'none'; // Fermer la popup
 
-	// Fonction pour ajouter une nouvelle option
-	function addNewOption() {
-		adding = true;
-		// Ajouter une nouvelle ligne vide pour l'édition
-		options.push(''); // Ajouter une nouvelle valeur vide (pour l'édition)
-		index(options.length - 1); // Mettre l'index sur la nouvelle valeur
-		input.setAttribute('data-value', JSON.stringify(options)); // Mettre à jour les multi-values
-		renderOptions(); // Rendre les options pour mettre à jour
-	}
+if (input.name === 'telephoneNumber') {
+console.log('->clickEvent: ');
+console.log('telephoneNumber: ', input.getAttribute('data-value'));
+}
+
+	}	});
 
 	// Gestionnaire d'événements
 	input.addEventListener('keydown', (event) => { // Ajouter une nouvelle option
 		if (event.key === 'Enter') { // Vérifier si la touche appuyée est <Enter>
-			exitInput(); input.focus();
-			event.preventDefault(); // Empêcher le comportement par défaut du bouton
-			addNewOption(); // Appeler la fonction pour ajouter une nouvelle option
+			adding = true;
+			options.push(input.value=''); index(options.length - 1); // Ajouter d'une nouvelle valeur
+			renderOptions(); // Rendre les options pour mettre à jour
 		} else if (event.key === 'Escape') {
 			if (adding) {
-				options.pop(); index(options.length-1); renderOptions();
-			} adding = false;
-		} else if (event.key === 'ArrowDown' || event.key === 'ArrowUp') { adding = false;
-			event.preventDefault(); // Empêcher le comportement par défaut du bouton
-			let i = index();
-			index ( (event.key === 'ArrowDown')
-				? (i + 1) % options.length
-				: (i - 1 + options.length) % options.length );	// Passer à l'option suivante ou précédente
-			renderOptions();
-		}
+				options.pop();
+				input.value = options[index(options.length-1)];
+				renderOptions();
+				adding = false;
+			}
+		} else if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+			if(!adding || options[index()].length) {
+				if (adding)
+					input.setAttribute('data-value', JSON.stringify(options)); // Mettre à jour les multi-values
+				adding = false;
+				event.preventDefault(); // Empêcher le comportement par défaut du bouton
+				let i = index();
+				index ( (event.key === 'ArrowDown')
+					? (i + 1) % options.length
+					: (i - 1 + options.length) % options.length );	// Passer à l'option suivante ou précédente
+				input.value = options[index()];
+				renderOptions();
+		}	}
 	});
 }
 
